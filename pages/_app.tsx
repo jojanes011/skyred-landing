0011a1c71ac759704afba5aef87be79085a6e618
Joan Oviedo <jojanes011@gmail.com>
2022-04-04 18:04:18 -0500

    Add building page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,31 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import Image from "next/image";
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const building = true;
+    
     return (
-        <Layout>
+        <>
+        {building ? (
+            <div className='bg-header bg-cover flex flex-col items-center text-center pt-40 space-y-8 h-screen'>
+                <Image
+                    className="cursor-pointer"
+                    width={148}
+                    height={42}
+                    alt='logo'
+                    src='/img/logo.png'
+                />
+                <h1 className="font-extrabold text-white text-5xl">Estamos en construcción</h1>
+                <i className="fas fa-hammer text-white text-5xl" />
+            </div>
+        ) : (
+            <Layout>
             <Component {...pageProps} />
-        </Layout>
+            </Layout>
+        )}
+    </>
     );
 }
 
